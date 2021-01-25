6fc3a512c23bac47d5b7009c0ba35bd1ec5010f8
Attila Babo <attila.babo@gmail.com>
2021-01-25 01:27:51 +0100

    Day 17b

--- 2020/d17/src/main.rs
+++ 2020/d17/src/main.rs
@@ -1,17 +1,21 @@
 use std::collections::HashSet;
 use std::fs::read_to_string;
 
-type Coord = i16;
-type Coords = (Coord, Coord, Coord);
+type Dim3 = (i16, i16, i16);
+type Dim4 = (i16, i16, i16, i16);
+
+trait Neighbours<T> {
+    fn neighbours(c: &T) -> Vec<T>;
+}
 
 fn content() -> Option<String> {
     read_to_string("./input.txt").ok()
 }
 
-fn to_hash(input: &str) -> HashSet<Coords> {
+fn to_hash<T: Eq + std::hash::Hash>(input: &str, conv: fn(i16, i16) -> T) -> HashSet<T> {
     let mut hs = HashSet::new();
 
-    let mut x: Coord = 0;
+    let mut x: i16 = 0;
 
     for line in input.lines() {
         line.trim()
@@ -19,36 +23,57 @@ fn to_hash(input: &str) -> HashSet<Coords> {
             .enumerate()
             .filter(|x| x.1 == '#')
             .for_each(|y| {
-                hs.insert((x, y.0 as Coord, 0 as Coord));
+                let t = conv(x, y.0 as i16);
+                hs.insert(t);
             });
         x += 1;
     }
     hs
 }
 
-fn neighbours(c: &Coords) -> [Coords; 26] {
-    let mut rtv: [Coords; 26] = [*c; 26];
-    let mut i = 0usize;
+impl Neighbours<Dim3> for Dim3 {
+    fn neighbours(c: &Dim3) -> Vec<Dim3> {
+        let mut rtv = Vec::new();
 
-    for x in c.0 - 1..=c.0 + 1 {
-        for y in c.1 - 1..=c.1 + 1 {
-            for z in c.2 - 1..=c.2 + 1 {
-                let n = (x, y, z);
-                if n != *c {
-                    rtv[i] = n;
-                    i += 1;
+        for x in c.0 - 1..=c.0 + 1 {
+            for y in c.1 - 1..=c.1 + 1 {
+                for z in c.2 - 1..=c.2 + 1 {
+                    let n = (x, y, z);
+                    if n != *c {
+                        rtv.push(n);
+                    }
                 }
             }
         }
+        rtv
     }
-    rtv
 }
 
-fn cycle(input: &HashSet<Coords>) -> HashSet<Coords> {
-    let mut hs = HashSet::new();
+impl Neighbours<Dim4> for Dim4 {
+    fn neighbours(c: &Dim4) -> Vec<Dim4> {
+        let mut rtv = Vec::new();
+
+        for x in c.0 - 1..=c.0 + 1 {
+            for y in c.1 - 1..=c.1 + 1 {
+                for z in c.2 - 1..=c.2 + 1 {
+                    for w in c.3 - 1..=c.3 + 1 {
+                        let n = (x, y, z, w);
+                        if n != *c {
+                            rtv.push(n);
+                        }
+                    }
+                }
+            }
+        }
+        rtv
+    }
+}
+
+fn cycle<T: Neighbours<T> + Copy + Eq + std::hash::Hash>(input: &HashSet<T>) -> HashSet<T> {
+    let mut hs = HashSet::<T>::new();
 
     for cube in input {
-        let nb = neighbours(cube);
+        let nb = T::neighbours(cube);
         let count = nb.iter().filter(|x| input.contains(*x)).count();
         match count {
             2 => {
@@ -60,7 +85,13 @@ fn cycle(input: &HashSet<Coords>) -> HashSet<Coords> {
             _ => (),
         }
         nb.iter().filter(|x| !input.contains(*x)).for_each(|x| {
-            if !hs.contains(x) && neighbours(x).iter().filter(|x| input.contains(*x)).count() == 3 {
+            if !hs.contains(x)
+                && T::neighbours(x)
+                    .iter()
+                    .filter(|x| input.contains(*x))
+                    .count()
+                    == 3
+            {
                 hs.insert(*x);
             }
         });
@@ -69,16 +100,28 @@ fn cycle(input: &HashSet<Coords>) -> HashSet<Coords> {
     hs
 }
 
+fn to_dim3(x: i16, y: i16) -> Dim3 {
+    (x, y, 0i16)
+}
+
+fn to_dim4(x: i16, y: i16) -> Dim4 {
+    (x, y, 0i16, 0i16)
+}
+
 fn solution_a(input: &str) -> usize {
-    let mut hs = to_hash(input);
+    let mut hs = to_hash::<Dim3>(input, to_dim3);
     for _ in 0..6 {
         hs = cycle(&hs);
     }
     hs.len()
 }
 
-fn solution_b(_input: &str) -> i32 {
-    0
+fn solution_b(input: &str) -> usize {
+    let mut hs = to_hash::<Dim4>(input, to_dim4);
+    for _ in 0..6 {
+        hs = cycle(&hs);
+    }
+    hs.len()
 }
 
 fn main() {
@@ -106,6 +149,7 @@ mod tests {
             ".#.
         ..#
         ###",
+            to_dim3,
         );
 
         assert_eq!(cycle(&input).len(), 11);
@@ -117,6 +161,7 @@ mod tests {
             ".#.
         ..#
         ###",
+            to_dim3,
         );
 
         for _ in 0..2 {
@@ -131,6 +176,7 @@ mod tests {
             ".#.
         ..#
         ###",
+            to_dim3,
         );
 
         for _ in 0..3 {
@@ -145,6 +191,7 @@ mod tests {
             ".#.
         ..#
         ###",
+            to_dim3,
         );
 
         for _ in 0..6 {
@@ -154,9 +201,31 @@ mod tests {
     }
 
     #[test]
+    fn test_dim4() {
+        let mut input = to_hash(
+            ".#.
+        ..#
+        ###",
+            to_dim4,
+        );
+
+        for _ in 0..6 {
+            input = cycle(&input);
+        }
+        assert_eq!(input.len(), 848);
+    }
+
+    #[test]
     fn test_solution_a() {
         let c = content().unwrap();
 
         assert_eq!(solution_a(&c), 280);
+    }
+
+    #[test]
+    fn test_solution_b() {
+        let c = content().unwrap();
+
+        assert_eq!(solution_b(&c), 1696);
     }
 }
