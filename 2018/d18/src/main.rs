ce50f65bb4cccf27049b98e732a0f668021c5642
Attila Babo <attila.babo@gmail.com>
2022-12-05 09:01:53 +0100

    Add 2018

---
+++ 2018/d18/src/main.rs
@@ -0,0 +1,69 @@
+use std::fs::read_to_string;
+
+fn content() -> Option<String> {
+    read_to_string("./input.txt").ok()
+}
+
+fn solve_a(line: &str) -> usize {
+    0
+}
+
+fn solve_b(line: &str) -> usize {
+    0
+}
+
+fn solution_a(input: &str) -> Option<usize> {
+    Some(input.lines().map(|x| solve_a(x.trim())).sum::<usize>())
+}
+
+fn solution_b(input: &str) -> Option<usize> {
+    Some(input.lines().map(|x| solve_b(x.trim())).sum::<usize>())
+}
+
+fn main() {
+    let c = content().unwrap();
+
+    let a = solution_a(&c);
+    let b = solution_b(&c);
+
+    println!("Step A: {:?}", a);
+    println!("Step B: {:?}", b);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn simple() -> Option<String> {
+        read_to_string("./simple.txt").ok()
+    }
+
+    #[test]
+    fn test_file_reading() {
+        assert_ne!(content().is_none(), true);
+    }
+
+    #[test]
+    fn test_simple_a() {
+        let data = simple().unwrap();
+        assert_eq!(solution_a(&data), Some(0));
+    }
+
+    #[test]
+    fn test_simple_b() {
+        let data = simple().unwrap();
+        assert_eq!(solution_b(&data), Some(0));
+    }
+
+    #[test]
+    fn test_solution_a() {
+        let c = content().unwrap();
+        assert_eq!(solution_a(&c), Some(0));
+    }
+
+    #[test]
+     fn test_solution_b() {
+        let c = content().unwrap();
+        assert_eq!(solution_b(&c), Some(0));
+    }
+}
