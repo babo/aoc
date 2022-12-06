5fdee17a95eaf660a2abc4ef4c28bfcf8bc955fd
Attila Babo <attila.babo@gmail.com>
2022-12-06 09:46:32 +0100

    Cleanup 2022/d06

--- 2022/d06/src/main.rs
+++ 2022/d06/src/main.rs
@@ -1,48 +1,26 @@
-use std::fs::read_to_string;
 use std::collections::HashSet;
-
-use itertools::Itertools;
+use std::fs::read_to_string;
 
 fn content() -> Option<String> {
     read_to_string("./input.txt").ok()
 }
 
-fn solve_a(line: &str) -> usize {
-    0
-}
-
-fn solve_b(line: &str) -> usize {
-    0
+fn solve_a(input: &str, n: usize) -> Option<usize> {
+    (0usize..(input.len() - n))
+        .position(|i| {
+            input
+                .get(i..(i + n))
+                .map_or(false, |x| HashSet::<char>::from_iter(x.chars()).len() == n)
+        })
+        .map(|x| x + n)
 }
 
 fn solution_a(input: &str) -> Option<usize> {
-    if input.len() < 4 {
-        None
-    } else {
-        let a = (0usize..input.len()).find_position(|i| {
-            input.get(*i..(*i+4)).map_or(false, |x| {
-                let s: HashSet<char> = HashSet::from_iter(x.chars());
-                s.len() == 4
-            })
-        });
-        println!("{:?}", a);
-        a.map(|x| x.0+4)
-    }
+    solve_a(input, 4)
 }
 
 fn solution_b(input: &str) -> Option<usize> {
-    if input.len() < 14 {
-        None
-    } else {
-        let a = (0usize..input.len()).find_position(|i| {
-            input.get(*i..(*i+14)).map_or(false, |x| {
-                let s: HashSet<char> = HashSet::from_iter(x.chars());
-                s.len() == 14
-            })
-        });
-        println!("{:?}", a);
-        a.map(|x| x.0+14)
-    }
+    solve_a(input, 14)
 }
 
 fn main() {
@@ -71,29 +49,34 @@ mod tests {
     #[test]
     fn test_simple_a() {
         let data = simple().unwrap();
-        let res = data.lines().map(|x| solution_a(x.trim()).map_or(0, |x| x)).sum::<usize>();
+        let res = data
+            .lines()
+            .map(|x| solution_a(x.trim()).map_or(0, |x| x))
+            .sum::<usize>();
 
-        assert_eq!(res, 7+5+6+10+11);
+        assert_eq!(res, 7 + 5 + 6 + 10 + 11);
     }
 
     #[test]
     fn test_simple_b() {
         let data = simple().unwrap();
-        let data = simple().unwrap();
-        let res = data.lines().map(|x| solution_a(x.trim()).map_or(0, |x| x)).sum::<usize>();
+        let res = data
+            .lines()
+            .map(|x| solution_b(x.trim()).map_or(0, |x| x))
+            .sum::<usize>();
 
-        assert_eq!(res, 7+5+6+10+11);
+        assert_eq!(res, 19 + 23 + 23 + 29 + 26);
     }
 
     #[test]
     fn test_solution_a() {
         let c = content().unwrap();
-        assert_eq!(solution_a(&c), Some(0));
+        assert_eq!(solution_a(&c), Some(1134));
     }
 
     #[test]
-     fn test_solution_b() {
+    fn test_solution_b() {
         let c = content().unwrap();
-        assert_eq!(solution_b(&c), Some(0));
+        assert_eq!(solution_b(&c), Some(2263));
     }
 }
