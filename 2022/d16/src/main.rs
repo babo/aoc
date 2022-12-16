9e98de6343cac33cc8926e21cf38a1886353fa57
Attila Babo <attila.babo@gmail.com>
2022-12-16 16:11:54 +0100

    wip

--- 2022/d16/src/main.rs
+++ 2022/d16/src/main.rs
@@ -1,5 +1,4 @@
 use std::collections::HashMap;
-use std::collections::HashSet;
 use std::fs::read_to_string;
 
 use itertools::Itertools;
@@ -8,12 +7,25 @@ fn content() -> Option<String> {
     read_to_string("./input.txt").ok()
 }
 
-struct Vulcano<'a> {
-    names: Vec<&'a str>,
-    rates: HashMap<&'a str, u32>,
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+struct EvalState {
+    valves: u64,
+    time: u8,
+    position: usize,
+    pressure: u32,
+    choices: [u8; 56],
 }
 
-impl<'a> Vulcano<'a> {
+struct Volcano<'a> {
+    names: Vec<&'a str>,
+    rates: HashMap<usize, u32>,
+    tunnel: HashMap<usize, Vec<usize>>,
+    start: usize,
+}
+
+impl<'a> Volcano<'a> {
+    const TIME_LIMIT: u8 = 30;
+
     // Valve YJ has flow rate=15; tunnels lead to valves OC, PE, AC
     fn new(input: &'a str) -> Self {
         let names: Vec<_> = input
@@ -22,70 +34,183 @@ impl<'a> Vulcano<'a> {
             .filter(|x| !x.is_empty())
             .map(|line| line.get(6..8).unwrap())
             .collect_vec();
+        let lookup: HashMap<&str, usize> =
+            HashMap::from_iter(names.iter().enumerate().map(|a| (*a.1, a.0)));
 
-        let mut rates: HashMap<&str, u32> = HashMap::new();
-        let mut tunnel: HashMap<&str, Vec<&str>> = HashMap::new();
-        let mut routes: HashMap<(&str, &str), u32> = HashMap::new();
+        let mut rates: HashMap<usize, u32> = HashMap::new();
+        let mut tunnel: HashMap<usize, Vec<usize>> = HashMap::new();
 
         input
             .lines()
             .map(|x| x.trim())
             .filter(|x| !x.is_empty())
             .for_each(|line| {
-                println!("{line}");
-                let name = line.get(6..8).unwrap();
+                let node = *lookup.get(line.get(6..8).unwrap()).unwrap();
                 let semi = line.find(';').unwrap();
                 let rate = u32::from_str_radix(line.get(23..semi).unwrap(), 10).unwrap();
                 let vp = line.find("valve").unwrap() + 5;
                 if line.get(vp..vp + 1) == Some("s") {
-                    let next: Vec<_> = line.get(vp + 2..).unwrap().split(", ").collect_vec();
-                    next.iter().for_each(|n| {
-                        routes.insert((name, n), 1);
-                        routes.insert((n, name), 1);
-                    });
-                    tunnel.insert(name, next);
+                    let next: Vec<_> = line
+                        .get(vp + 2..)
+                        .unwrap()
+                        .split(", ")
+                        .map(|name| *lookup.get(name).unwrap())
+                        .collect();
+                    tunnel.insert(node, next);
                 } else {
-                    let mut next = Vec::<&str>::new();
-                    next.push(line.get(vp + 1..).unwrap());
-                    tunnel.insert(name, next);
+                    let mut next = Vec::new();
+                    next.push(*lookup.get(line.get(vp + 1..).unwrap()).unwrap());
+                    tunnel.insert(node, next);
                 }
-                rates.insert(name, rate);
+                rates.insert(node, rate);
             });
-        let mut visited = HashSet::new();
-        let mut current: Vec<usize> = Vec::new();
-        let mut next: Vec<usize> = Vec::new();
-        current.push(0);
 
-        while !current.is_empty() {
-            next.clear();
-            current.iter().for_each(|node_id| {
-                if visited.insert(*node_id) {
-                    let node = names[*node_id];
-                    for k in tunnel[node].iter() {
-                        println!("|{k}|");
-                        let pos = names.iter().position(|x| x == k).unwrap();
-                        next.push(pos);
+        let start = *lookup.get("AA").unwrap();
+        Volcano {
+            names,
+            rates,
+            tunnel,
+            start,
+        }
+    }
+
+    fn initial_state(&self) -> EvalState {
+        let choices: Vec<_> = (0..56)
+            .map(|index| {
+                if index < self.names.len() {
+                    let mut c = 0;
+                    for i in 0..self.tunnel.get(&index).unwrap().len() {
+                        c |= 1 << i;
                     }
+                    c
+                } else {
+                    0
                 }
-            });
+            })
+            .collect();
+        let choices: [u8; 56] = choices.try_into().unwrap();
+        EvalState {
+            valves: 0,
+            time: 1,
+            position: self.start,
+            pressure: 0,
+            choices,
+        }
+    }
 
-            current.clear();
-            current.extend(next.iter());
+    fn take_action(&self, state: &EvalState) -> Vec<EvalState> {
+        let mut next = Vec::new();
+        let current_pos = state.position;
+        let current_time = state.time;
+        let free_choices = state.choices[current_pos];
+        let name = self.names[state.position];
+        let mut r = 0;
+        for (i, n) in self.names.iter().enumerate() {
+            if state.valves & (1 << i) != 0 {
+                if r == 0 {
+                    print!("Valves at {current_time} ");
+                }
+                r += self.rates.get(&i).unwrap();
+                print!("{n} ");
+            }
         }
-        for (k, v) in tunnel.iter() {
-            println!("{k} -> {:?}", v);
+        if r != 0 {
+            println!("{r} total {}", state.pressure);
         }
-        Vulcano { names, rates }
+        if current_time <= Self::TIME_LIMIT && free_choices != 0 {
+            println!("Free choices at {}: {:04b}", name, free_choices);
+            let rate = *self.rates.get(&current_pos).unwrap();
+            let is_closed = (state.valves & (1 << current_pos)) == 0;
+            let mut new_choice = state.choices.clone();
+
+            for (next_choice, next_position) in
+                self.tunnel.get(&current_pos).unwrap().iter().enumerate()
+            {
+                if free_choices & (1 << next_choice) != 0 {
+                    let next_position = *next_position;
+                    let turned_off = free_choices & !(1 << next_choice);
+                    new_choice[current_pos] = turned_off;
+                    if rate > 0 && is_closed && Volcano::TIME_LIMIT >= current_time {
+                        let valves = state.valves | (1 << current_pos);
+                        let increased = rate * (Volcano::TIME_LIMIT as u32 - current_time as u32);
+
+                        println!(
+                            "Moving from {} to {} at {} increase from {} with {} at rate {} for {}",
+                            name,
+                            self.names[next_position],
+                            current_time,
+                            state.pressure,
+                            increased,
+                            rate,
+                            increased / rate
+                        );
+                        next.push(EvalState {
+                            valves,
+                            time: current_time + 2,
+                            position: next_position,
+                            pressure: state.pressure + increased,
+                            choices: new_choice,
+                        });
+                    }
+                    println!(
+                        "Moving from {} to {} at {}",
+                        name, self.names[next_position], current_time
+                    );
+                    next.push(EvalState {
+                        valves: state.valves,
+                        time: current_time + 1,
+                        position: next_position,
+                        pressure: state.pressure,
+                        choices: new_choice,
+                    });
+                }
+            }
+        } else {
+            println!("Over: {} {} {}", name, state.time, free_choices);
+        }
+        next
+    }
+
+    fn show(&self, state: &EvalState) {
+        println!(
+            "Position: {} at {} with {}",
+            self.names[state.position as usize], state.time, state.pressure
+        );
     }
 }
 
 fn solution_a(input: &str) -> usize {
-    let v = Vulcano::new(input);
-    println!("{:?}", v.names);
-    for (k, v) in v.rates.iter() {
-        println!("{k} -> {v}");
+    let v = Volcano::new(input);
+
+    let mut current_states = Vec::new();
+    let mut future_states = Vec::new();
+    let mut best_so_far = 0u32;
+    current_states.push(v.initial_state());
+
+    while !current_states.is_empty() {
+        future_states.clear();
+        current_states.iter().for_each(|state| {
+            v.show(state);
+            v.take_action(state).iter().for_each(|next| {
+                if next.pressure > best_so_far {
+                    best_so_far = next.pressure;
+                }
+
+                if next.choices.iter().fold(0, |prev, x| -> u8 { prev | *x }) == 0u8 {
+                    println!(
+                        "Done: {} at {} with {}",
+                        v.names[next.position], next.time, next.pressure
+                    );
+                } else {
+                    future_states.push(*next);
+                }
+            });
+        });
+        current_states.clear();
+        current_states.extend(future_states.iter());
     }
-    0
+
+    best_so_far as usize
 }
 
 fn solution_b(_input: &str) -> usize {
