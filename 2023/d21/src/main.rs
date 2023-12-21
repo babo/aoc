23e1226d6c68cb56f966ab2004b353979a96d941
Attila Babo <attila.babo@gmail.com>
2023-12-21 07:24:17 +0100

    d21 - part 2 wip

--- 2023/d21/src/main.rs
+++ 2023/d21/src/main.rs
@@ -6,10 +6,6 @@ fn content() -> Option<String> {
     read_to_string("./input.txt").ok()
 }
 
-fn solve_b(_line: &str) -> usize {
-    0
-}
-
 fn solution_a(input: &str, steps: usize) -> Option<usize> {
     let input = input
         .lines()
@@ -47,19 +43,49 @@ fn solution_a(input: &str, steps: usize) -> Option<usize> {
             )
         },
     );
-    hs.iter().for_each(|xy| assert_ne!(input[at(xy)], '#'));
     Some(hs.len())
 }
 
-fn solution_b(input: &str) -> Option<usize> {
-    Some(input.lines().map(|x| solve_b(x.trim())).sum::<usize>())
+fn solution_b(input: &str, steps: usize) -> Option<usize> {
+    let input = input
+        .lines()
+        .map(|x| x.trim())
+        .filter(|x| !x.is_empty())
+        .join("\n");
+    let w = input.lines().next().map(|x| x.len()).unwrap();
+    let input = input
+        .chars()
+        .filter(|x| !x.is_ascii_whitespace())
+        .collect_vec();
+    let h = input.len() / w;
+    let s = input.iter().position(|x| *x == 'S').unwrap();
+    let s = ((s % w) as i64, (s / w) as i64);
+
+    let at = |(x, y): &(i64, i64)| {
+        ((w as i64 + x % w as i64).abs() as usize) % w
+            + (((h as i64 + y % h as i64).abs() as usize) % h) * w
+    };
+
+    let hs: HashSet<(i64, i64)> = (0..steps).fold(
+        HashSet::from_iter(std::iter::repeat(s).take(1)),
+        |current, _step| {
+            HashSet::from_iter(
+                current
+                    .iter()
+                    .map(|(x, y)| vec![(*x - 1, *y), (*x + 1, *y), (*x, *y + 1), (*x, *y - 1)])
+                    .flatten()
+                    .filter(|xy| input[at(xy)] != '#'),
+            )
+        },
+    );
+    Some(hs.len())
 }
 
 fn main() {
     let c = content().unwrap();
 
     let a = solution_a(&c, 64);
-    let b = solution_b(&c);
+    let b = solution_b(&c, 26501365);
 
     println!("Step A: {:?}", a);
     println!("Step B: {:?}", b);
@@ -87,7 +113,18 @@ mod tests {
     #[test]
     fn test_simple_b() {
         let data = simple().unwrap();
-        assert_eq!(solution_b(&data), Some(0));
+        let steps = vec![
+            (6, 16),
+            (10, 50),
+            (50, 1594),
+            (100, 6536),
+            (500, 167004),
+            (1000, 668697),
+            (5000, 16733044),
+        ];
+        steps
+            .iter()
+            .for_each(|(steps, result)| assert_eq!(solution_b(&data, *steps), Some(*result)));
     }
 
     #[test]
@@ -99,6 +136,6 @@ mod tests {
     #[test]
     fn test_solution_b() {
         let c = content().unwrap();
-        assert_eq!(solution_b(&c), Some(0));
+        assert_eq!(solution_b(&c, 26501365), Some(0));
     }
 }
